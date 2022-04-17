38188ac0b94c9cc874d3204fd9b9bb0a73d20705
Jocque Jefferson <jocque.jefferson@gmail.com>
2022-04-17 13:29:17 -0700

    menu commit

--- src/main.js
+++ src/main.js
@@ -12,4 +12,4 @@ let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 
 // reserve keyboard vars
-let keyF, keyLeft, keyRight, keyO, keyA, keyD, keyUP, keyDOWN;
+let keyF, keyLEFT, keyRIGHT, keyO, keyA, keyD, keyUP, keyDOWN;
