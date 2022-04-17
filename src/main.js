1c4428f017bae22afcca3e52fa0fcfc2b098a6ba
Jocque Jefferson <jocque.jefferson@gmail.com>
2022-04-17 13:54:38 -0700

    main mod

--- src/main.js
+++ src/main.js
@@ -1,15 +1,34 @@
+/*
+Thomas Applewhite
+All work is my own or provided from the tutorial
+---Point Breakdown---
+S-Tier:
+    simultaneous two-player mode: 50 points
+Intermediate Tier:
+    new timing/scoring mechanism that adds time to the clock for successful hits: 25 points
+Novice Tier:
+    display the time remaining (in seconds) on the screen: 15 points
+Starting Tier:
+    speed increase that happens after 30 seconds in the original game: 10 points
+    allow the player to control the Rocket after it's fired: 10 points
+Final Score: 110/100
+I know I can't go over 100 but I forgot the timer was on the list so I went over by accident
+*/
+
+
 let config = {
-  type: Phaser.AUTO,
+  type: Phaser.CANVAS,
   width: 640,
   height: 480,
-  scene: [Menu, MenuMulti, Play]
+  scene: [ Menu, MenuMulti, Play ]
 }
 
 let game = new Phaser.Game(config);
+game.settings = {
+  spaceshipSpeed: 3,
+  gameTimer: 60000,
+  multiplay: 0
+}
 
-// set UI sizes
-let borderUISize = game.config.height / 15;
-let borderPadding = borderUISize / 3;
-
-// reserve keyboard vars
-let keyF, keyLEFT, keyRIGHT, keyO, keyA, keyD, keyUP, keyDOWN;
+//reserve keyboard vars
+let keyF, keyLEFT, keyRIGHT, keyS, keyN, keyM, keyUP, keyDOWN;
