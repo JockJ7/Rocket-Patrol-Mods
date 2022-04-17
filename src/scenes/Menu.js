21dada73bb0d82809e091b85f5129a985df341b4
Jocque Jefferson <jocque.jefferson@gmail.com>
2022-04-17 13:55:29 -0700

    menu mod

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -3,62 +3,68 @@ class Menu extends Phaser.Scene {
         super("menuScene");
     }
 
-    preload() {
-        // load audio
-        this.load.audio('sfx_select', './Assets/blip_select12.wav');
-        this.load.audio('sfx_explosion', './Assets/explosion38.wav');
-        this.load.audio('sfx_rocket', './Assets/rocket_shot.wav');
-      }
+    preload(){
+        this.load.audio('sfx_select', './assets/blip_select12.wav');
+        this.load.audio('sfx_explosion', './assets/explosion38.wav');
+        this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
+    }
 
     create() {
-        // menu text configuration
         let menuConfig = {
-            fontFamily: 'Courier',
-            fontSize: '28px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
-            align: 'right',
-            padding: {
+        fontFamily: 'Courier',
+        fontSize: '28px',
+        backgroundColor: '#F3B141',
+        color: '#843605',
+        align: 'right',
+        padding: {
             top: 5,
-            bottom: 5,
-            },
-            fixedWidth: 0
-        }
+            bototm: 5,
+        },
+        fixedWidth: 0
+        }   
 
         //show menu text
-        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2, 'Use <--> arrows to move & (f) to fire', menuConfig).setOrigin(0.5);
+        let centerX = game.config.width/2;
+        let centerY = game.config.height/2;
+        let textSpacer = 64;
+
+        this.add.text(centerX, centerY - textSpacer, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY, 'Use ↔ arrows to move and (F) to Fire', menuConfig).setOrigin(0.5);
         menuConfig.backgroundColor = '#00FF00';
         menuConfig.color = '#000';
-        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press <- for Novice or -> for Expert', menuConfig).setOrigin(0.5);
-//show menu
-        // define keys
+        this.add.text(centerX, centerY + textSpacer, 'Press ← for Easy or → for Hard', menuConfig).setOrigin(0.5);
+        menuConfig.backgroundColor = '#3DBAFF';
+        menuConfig.color = "#FFF";
+        this.add.text(centerX, centerY + (2 * textSpacer), 'Press ↑ for Two Player Mode', menuConfig).setOrigin(0.5);
+        
+        //define keys
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
         keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-
+        
+        //this.scene.start("playScene");
     }
 
-    update() {
-        if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
-            // easy mode
+    update(){
+        if(Phaser.Input.Keyboard.JustDown(keyLEFT)){
+            //easy mode
             game.settings = {
-            spaceshipSpeed: 3,
-            gameTimer: 60000,
-            multiplay: 0   
+                spaceshipSpeed: 3,
+                gameTimer: 60000,
+                multiplay: 0
             }
             this.sound.play('sfx_select');
-            this.scene.start('playScene');    
+            this.scene.start("playScene");
         }
-        if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
-            // hard mode
+        if(Phaser.Input.Keyboard.JustDown(keyRIGHT)){
+            //hard mode
             game.settings = {
-            spaceshipSpeed: 4,
-            gameTimer: 45000, 
-            multiplay: 0   
+                spaceshipSpeed: 4,
+                gameTimer: 45000,
+                multiplay: 0
             }
             this.sound.play('sfx_select');
-            this.scene.start('playScene');    
+            this.scene.start("playScene");
         }
         if(Phaser.Input.Keyboard.JustDown(keyUP)){
             //go to two-player mode
