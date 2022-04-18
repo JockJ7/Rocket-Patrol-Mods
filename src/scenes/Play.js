cb8d0a9b7061f0bac63ce9faf5eb7fe9134a48f7
Jocque Jefferson <jocque.jefferson@gmail.com>
2022-04-17 19:30:48 -0700

    fast ship

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,6 +10,7 @@ class Play extends Phaser.Scene {
       this.load.image('rocketRed', './assets/rocketRed.png');
       this.load.image('spaceship', './assets/spaceship.png');
       this.load.image('starfield', './assets/starfield.png');
+      this.load.image('fastship', './assets/fastShip.png');
       // load spritesheet
       this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64,
       frameHeight: 32, startFrame: 0, endFrame: 9});
