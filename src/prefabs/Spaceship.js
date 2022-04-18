d4eb6922954ba4ed2eabab100f0623db39f74894
Jocque Jefferson <jocque.jefferson@gmail.com>
2022-04-18 12:50:17 -0700

    commit spaceship mod

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -1,20 +1,23 @@
 class Spaceship extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame, pointValue){
         super(scene, x, y, texture, frame);
-        scene.add.existing(this);
+
+        // add object to existing scene
+        scene.add.existing(this); // add to existing, displayList, updateList
         //store pointValue
-        this.points = pointValue;
+        this.points = pointValue; // track rocket's firing status
     }
 
     update(){
         //move spaceship left
         this.x -= game.settings.spaceshipSpeed;
-        //wraparound from left to right edge
+        //wrap around from left to right edge
         if(this.x <= 0-this.width){
             this.reset();
         }
     }
 
+    //position reset
     reset() {
         this.x = game.config.width;
     }
