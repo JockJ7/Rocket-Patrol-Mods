406456a404491d59cbda54a1b6bd8d100b4bcbb0
Jocque Jefferson <jocque.jefferson@gmail.com>
2022-04-18 18:00:08 -0700

    2nd commit

--- src/main.js
+++ src/main.js
@@ -13,7 +13,9 @@
 //-Allow the player to control the Rocket after it's fired (5);
 
 //-Create a new scrolling tile sprite for the background (5);
+
 //-Implement the speed increase that happens after 30 seconds in the original game (5);
+
 //-Track a high score that persists across scenes and display it in the UI (5);
 //Total:(100) Points
 
